2a7b0c1fd8a18cdca90abd3185da50049a05bca1
jaeyun0000 <jjy20060022@gmail.com>
2026-09-02 14:51:12 +0900

    feat: 총알 양쪽 발사

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -7,7 +7,8 @@ public class PlayerFire : MonoBehaviour
     // - 총알 프리팹
     public GameObject BulletPrefab;
     // - 생성 위치(총구)
-    public Transform FirePoint;
+    public Transform FirePoint_1;
+    public Transform FirePoint_2;
     
     
     private void Update()
@@ -16,8 +17,10 @@ public class PlayerFire : MonoBehaviour
         {
             // 2. 총알 프리팹을 생성한다
             // Instantiate = 프리팹을 복사해서 (Monobehaviour를 상속받은)게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = FirePoint.position; // 생성한 총알의 위치를 총구의 위치로
+            GameObject bullet1 = Instantiate(BulletPrefab);
+            GameObject bullet2 = Instantiate(BulletPrefab);
+            bullet1.transform.position = FirePoint_1.position; // 생성한 총알의 위치를 총구의 위치로
+            bullet2.transform.position = FirePoint_2.position;
         }
     }
 }
