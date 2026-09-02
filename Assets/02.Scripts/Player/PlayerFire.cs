674e25f8bf7f988d00cb476ddacd60b6209cf9f7
jaeyun0000 <jjy20060022@gmail.com>
2026-09-02 15:37:28 +0900

    feat: 자동 공격 모드

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -12,6 +12,8 @@ public class PlayerFire : MonoBehaviour
 
     private float cooldown = 1f;
     private float cooldownTimer = 0f;
+
+    private int autoAttack = 0;
     
     
     private void Update()
@@ -20,14 +22,37 @@ public class PlayerFire : MonoBehaviour
         {
             cooldownTimer -= Time.deltaTime;
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (autoAttack == 0)
+            {
+                autoAttack = 1;
+            }
+            else if (autoAttack == 1)
+            {
+                autoAttack = 0;
+            }
+        }
         
-        if (Input.GetKeyDown(KeyCode.Space) && cooldownTimer <= 0)
+        if (Input.GetKeyDown(KeyCode.Space) && autoAttack == 0 && cooldownTimer <= 0)
         {
             // 2. 총알 프리팹을 생성한다
             // Instantiate = 프리팹을 복사해서 (Monobehaviour를 상속받은)게임 오브젝트를 생성하고 씬에 넣어주는 기능
             GameObject leftBullet = Instantiate(BulletPrefab);
             GameObject rightBullet = Instantiate(BulletPrefab);
             leftBullet.transform.position = LeftFirePoint.position; // 생성한 총알의 위치를 총구의 위치로
+            rightBullet.transform.position = RightFirePoint.position;
+            
+            cooldownTimer = cooldown;
+        }
+        
+        if (autoAttack == 1 && cooldownTimer <= 0)
+        {
+            GameObject leftBullet = Instantiate(BulletPrefab);
+            GameObject rightBullet = Instantiate(BulletPrefab);
+            
+            leftBullet.transform.position = LeftFirePoint.position;
             rightBullet.transform.position = RightFirePoint.position;
             
             cooldownTimer = cooldown;
