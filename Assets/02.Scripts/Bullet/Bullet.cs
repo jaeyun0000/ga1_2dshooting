88c5cb575500560ef356a980b54bcc030cf93f10
jaeyun0000 <jjy20060022@gmail.com>
2026-09-02 14:41:38 +0900

    feat: 총알 발사(생성 및 이동)

---
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float Speed = 8f;
+    
+    private void Update()
+    {
+        Vector2 direction = Vector2.up; // new Vector2(1, 0);
+        transform.Translate(direction * Speed * Time.deltaTime);
+    }
+}
