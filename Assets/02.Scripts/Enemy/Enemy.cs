de53147232237f7ff65a428cb2d9df0a1ef841b6
jaeyun0000 <jjy20060022@gmail.com>
2026-09-07 13:01:27 +0900

    chore: Todo 추가

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -49,6 +49,9 @@ public abstract class Enemy : MonoBehaviour
 
     private void Die()
     {
+        // Todo: Scrptable Object를 사용해서 리팩토링
+        // 이유1: 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알 수가 없음
+        // 이유2: 각 Enemy 스폰 확률을 매직 넘버로 하드코딩해서 유지보수가 어렵고 가독성 저하
         int dropItem = UnityEngine.Random.Range(0, _itemPrefabs.Length);
         Vector2 dropPosition = transform.position;
 
