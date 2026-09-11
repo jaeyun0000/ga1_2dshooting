62ac5ef186c74602ad03a82ef49842353d8ccb12
jaeyun0000 <jjy20060022@gmail.com>
2026-09-11 10:46:41 +0900

    feat: 적 스폰 확률 데이터 테이블
    - 데이터 테이블을 SO로 만듦

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -8,16 +8,14 @@ public class EnemySpawner : MonoBehaviour
     [Header("스폰 간격")]
     [SerializeField] private float _spawnInterval = 3f;
 
+    [SerializeField] private EnemySpawnDataTableSO _spawnDataTable;
+
     private float _timer;
 
-    // - 생성할 프리팹
-    [Header("스폰할 적 프리팹")]
-    [SerializeField] private Enemy[] _enemyPrefabs;
 
     private void Update()
     {
         _timer += Time.deltaTime;
-
 
         if (_timer >= _spawnInterval)
         {
@@ -42,20 +40,31 @@ public class EnemySpawner : MonoBehaviour
         // Todo: Scrptable Object를 사용해서 리팩토링
         // 이유1: 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알 수가 없음
         // 이유2: 각 Enemy 스폰 확률을 매직 넘버로 하드코딩해서 유지보수가 어렵고 가독성 저하
-        if (spawnEnemy <= 20)
+
+        // 가중치 랜덤 선택
+        // 각 아이템에 가중치를 부여하고, 가중치가 클수록 높은 확률로 선택되도록 하는 방식
+
+        // 1. 추청할 수 있는 모든 가중치를 더한다.
+        int totalWeight = 0;
+        foreach (EnemySpawnData data in _spawnDataTable.Datas)
         {
-            enemyIndex = 0;
-        }
-        else if (spawnEnemy <= 50)
-        {
-            enemyIndex = 1;
-        }
-        else if (spawnEnemy <= 100)
-        {
-            enemyIndex = 2;
+            totalWeight += data.Weight;
         }
 
-        Enemy enemy = Instantiate(_enemyPrefabs[enemyIndex]);
-        enemy.transform.position = transform.position;
+        // 2. 전체 가중치 범위에서 랜검한 정수를 뽑는다.
+        int randomWeight = Random.Range(0, totalWeight);
+
+        // 3. 가중치를 누적하면서 선택된 구간을 찾는다.
+        int cumulativeWeight = 0;
+        foreach (EnemySpawnData data in _spawnDataTable.Datas)
+        {
+            cumulativeWeight += data.Weight; // 누적
+            if (randomWeight < cumulativeWeight) // 구간
+            {
+                GameObject enemy = Instantiate(data.EnemyPrefab);
+                enemy.transform.position = transform.position;
+                break;
+            }
+        }
     }
 }
